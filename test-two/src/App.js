fdf277eb3d85ad2502ecb0f406d21d02ef7fe17a
Oscar Pacheco <Oscarpa88@yahoo.com>
2019-05-21 17:12:22 -0500

    Created app, component folders and files

---
+++ test-two/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header"> Baseball Stadium Peronnel</header> 
+    </div>
+  );
+}
+
+export default App;
